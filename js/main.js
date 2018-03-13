379a800499da091315c39594cc48a9e61e8de92c
ShV0 <sd.virus@yandex.by>
2018-03-13 20:31:24 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ var canvas = document.querySelector('canvas');
 var paint = canvas.getContext('2d');
 var bm = document.querySelector('.bm');
 var bm2 = document.querySelector('.bm2');
+var bm3 = document.querySelector('.bm3');
 var button = document.querySelector('.buttonStart');
+var title = document.querySelector('.title');
+var record = document.querySelector('.record');
 
 (function() {
   var requestAnimationFrame = 
@@ -13,6 +16,8 @@ var button = document.querySelector('.buttonStart');
   window.requestAnimationFrame = requestAnimationFrame;
 
 })();
+
+record.innerHTML = localStorage.getItem('record');
 
 /* GAME */
 
@@ -46,18 +51,10 @@ function createBalls() {
 
         var dx, dy;
 
-        if(count >= 50 && count <= 99) {
+        if(count >= 50) {
             dx = 4,
             dy = 4;
         }
-        else if(count >= 100 && count <= 149 ) {
-            dx = 5,
-            dy = 5;
-        }
-        else if(count >= 150) {
-            dx = 7,
-            dy = 7;
-        } 
         else {
             dx = 3,
             dy = 3;
@@ -69,16 +66,18 @@ function createBalls() {
     }
 }
 
+localStorage.setItem('record', localStorage.getItem('record'));
+
 function drawBalls() {
 
     paint.clearRect(0,0,window.innerWidth, window.innerHeight);
 
     block();
-    if(yBlock >= (window.innerHeight + 40) ) {
-        yBlock = window.innerHeight;
+    if(yBlock >= (window.innerHeight - 40) ) {
+        yBlock = window.innerHeight - 40;
     }
     else if(yBlock <= 0) {
-        yBlock += 10;   
+        yBlock = 1;   
     }
     else {
         yBlock += 2;
@@ -94,21 +93,22 @@ function drawBalls() {
 
             if(xBlock >= (one.x - 40) && xBlock <= (one.x + 40)) {
 
+                if(localStorage.getItem('record') < count) {
+                    localStorage.setItem('record', count);
+                    record.innerHTML = localStorage.getItem('record');
+                }
+
                 fail();
 
             }
 
         }
 
-        if(yBlock >= window.innerHeight || yBlock <= 0) {
-
-            fail();
-
-        }
-
         //drawing
-
-        paint.fillStyle = 'rgba(0,0,255,0.35)';
+        paint.shadowColor = 'rgba(255,255,255,0.3)';
+        paint.shadowBlur = 15;
+        paint.shadowOffsetX = -15;
+        paint.fillStyle = 'rgba(255,255,255,0.35)';
         paint.fillRect(one.x,one.y,one.w,one.h);
 
     }
@@ -123,14 +123,17 @@ var yBlock = (window.innerHeight / 2) + 20;
 
 function block() {
 
-    paint.fillStyle = 'rgba(0,255,0,0.3)';
+    paint.shadowColor = 'rgba(0,0,0,0.3)';
+    paint.shadowBlur = 30;
+    paint.shadowOffsetX = 0;
+    paint.fillStyle = 'rgba(255, 235, 59, 0.40)';
     paint.fillRect(xBlock,yBlock,40,40);
 
 }
 
 function fail() {
 
-    bm.innerHTML = '<span style="color:rgba(255,0,0,0.3)">FAIL</span>';
+    bm.innerHTML = '<span style="color:rgba(255,255,255,0.3)">0</span>';
     count = 0;
 
 }
@@ -144,7 +147,7 @@ addEventListener('keydown', function() {
 });
 
 addEventListener('touchend', function() {
-    yBlock -= 40;
+    yBlock -= 45;
 });
 
 
@@ -154,6 +157,7 @@ function generate() {
     requestAnimationFrame(generate);
 
     if(timer > check) {
+
         createBalls();
         count++;
         timer = 0;
@@ -169,9 +173,17 @@ function generate() {
 window.onload = function() {
 
     bm.style.display = 'none';
+    bm2.style.display = 'block';
+
+    bm3.onclick = function() {
+        location.reload();
+    }
 
     button.onclick = function() {
         button.style.display = 'none';
+        bm.style.display = 'block';
+        bm3.style.display = 'block';
+        title.style.display = 'none';
 
         block();
         createBalls();
@@ -182,8 +194,24 @@ window.onload = function() {
 
 /* /MENU */
 
-/*setInterval(function() {
-createBalls();
-count++;
-bm.innerHTML = count;
-}, 500);*/
+/* BACKGROUND */
+
+var canvasBack = document.querySelector('.canvasTwo');
+var paintBack = canvasBack.getContext('2d');
+
+canvasBack.width = window.innerWidth;
+canvasBack.height = window.innerHeight;
+
+var countStars = 450,
+    x, y, sq;
+
+for(var i = 0; i < countStars; i++) {
+
+    x = Math.random() * window.innerWidth;
+    y = Math.random() * window.innerHeight;
+    sq = Math.random() * 2;
+
+    paintBack.fillStyle = 'rgba(255,255,255,'+ (Math.random() * 1) +')';
+    paintBack.fillRect(x,y,sq,sq);
+
+}
